5478d26d5ef85fee2c19fa1e6c1692f301bcf113
Nevsimao <coday.ink@gmail.com>
2021-05-06 23:14:21 -0400

    Started the home comp

---
+++ src/components/HomeComp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {
+    Home
+} from '../styles/Home/homeStyles'
+
+const HomeComp = () => {
+    return (
+        <Home>The Sexy Code God</Home>
+    )
+}
+
+export default HomeComp
