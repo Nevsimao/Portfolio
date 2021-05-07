9914a464a8e66ecebf5b29a41d1a00a940cc2e95
Nevsimao <coday.ink@gmail.com>
2021-05-07 01:01:35 -0400

    added structure to the page

---
+++ src/components/sections/Main/SectionsComp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {
+    Sections
+} from '../../../styles/Styles'
+
+const SectionsComp = () => {
+    return (
+        <Sections></Sections>
+    )
+}
+
+export default SectionsComp
