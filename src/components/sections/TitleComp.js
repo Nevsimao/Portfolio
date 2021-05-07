53dcfd3adaf268c751039e0844c46d9c74dbaa66
Nevsimao <coday.ink@gmail.com>
2021-05-07 01:00:40 -0400

    added structure to the page

---
+++ src/components/sections/TitleComp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {
+    Title,
+    TitleName,
+    TitleSub
+} from '../../styles/Styles'
+
+const TitleComp = () => {
+    return (
+        <Title>
+            <TitleName>SIMAO NEVES</TitleName>
+            <TitleSub>SOFTWARE ENGINEER</TitleSub>
+        </Title>
+    )
+}
+
+export default TitleComp
