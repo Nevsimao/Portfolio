9b67ba16a8e709f9192df5a0d5e0a8266f0b3dc6
Nevsimao <coday.ink@gmail.com>
2021-05-07 00:59:03 -0400

    added all required comps

--- src/components/HomeComp.js
+++ src/components/HomeComp.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import LinksComp from './sections/LinksComp'
+import TitleComp from './sections/TitleComp'
+import MainComp from './sections/Main/MainComp'
+
 import {
     Home
 } from '../styles/Styles'
 
 const HomeComp = () => {
     return (
-        <Home>The Sexy Code God</Home>
+        <Home>
+            <LinksComp />
+            <TitleComp />
+            <MainComp />
+        </Home>
     )
 }
 
