566199d5729723f2d324f7486148c8c0b95f8367
Nevsimao <coday.ink@gmail.com>
2021-05-06 23:16:03 -0400

    home comp styles

---
+++ src/styles/Home/homeStyles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Home = styled.section`
+    width: 100vw;
+    height: 100vh;
+    background: black;
+    color: white;
+    text-align: center
+`
