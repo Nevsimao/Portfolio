e048af8ee061fb7c9845a225771b78e570e0bd1e
Nevsimao <coday.ink@gmail.com>
2021-05-09 03:14:07 -0400

    clean up

--- src/components/sections/LinksComp.js
+++ src/components/sections/LinksComp.js
@@ -10,11 +10,11 @@ import {
 const LinksComp = () => {
     return (
         <Links>
-            <StyledLink to={{ pathname: 'https://github.com/Nevsimao03' }} target="_blank">
-                <GitIcon size="28" />
+            <StyledLink to={{ pathname: 'https://github.com/Nevsimao03' }} target='_blank'>
+                <GitIcon size='28' />
             </StyledLink>
-            <StyledLink to={{ pathname: 'https://www.linkedin.com/in/simao-neves/' }} target="_blank">
-                <LinkedInIcon size="28" />
+            <StyledLink to={{ pathname: 'https://www.linkedin.com/in/simao-neves/' }} target='_blank'>
+                <LinkedInIcon size='28' />
             </StyledLink>
         </Links>
     )
