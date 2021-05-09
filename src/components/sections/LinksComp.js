41cf32efab25b830b29b04fe6119550ad6b1da2f
Nevsimao <coday.ink@gmail.com>
2021-05-09 02:59:29 -0400

    added the pathname for the StyledLink

--- src/components/sections/LinksComp.js
+++ src/components/sections/LinksComp.js
@@ -10,10 +10,10 @@ import {
 const LinksComp = () => {
     return (
         <Links>
-            <StyledLink to='/'>
+            <StyledLink to={{ pathname: 'https://github.com/Nevsimao03' }} target="_blank">
                 <GitIcon size="28" />
             </StyledLink>
-            <StyledLink to='/'>
+            <StyledLink to={{ pathname: 'https://www.linkedin.com/in/simao-neves/' }} target="_blank">
                 <LinkedInIcon size="28" />
             </StyledLink>
         </Links>
