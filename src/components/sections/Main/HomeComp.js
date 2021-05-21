1687cdb52e18fd98550351d45a05dcb98171e90c
Nevsimao <coday.ink@gmail.com>
2021-05-21 14:04:52 -0400

    fixed up little probs

--- src/components/sections/Main/HomeComp.js
+++ src/components/sections/Main/HomeComp.js
@@ -7,13 +7,15 @@ import { Summary } from '../../../styles/Styles'
 const HomeComp = () => {
     return (
         <Summary>
-            <Typewriter
-                string="I'm a Software Engineer who enjoys writing clean code while turning complex problems into simple solutions!"
-                delay={50}
-                cursor='_'
-                cursorClassName
-                stopBlinkinOnComplete
-            />
+                <h3 style={{padding: '18% 1em'}}>
+                    <Typewriter
+                        string="Hi! I'm Simao, a Software Engineer who enjoys writing clean code while turning complex problems into simple solutions. Look around to learn more!"
+                        delay={50}
+                        cursor='_'
+                        cursorClassName
+                        stopBlinkinOnComplete
+                    />
+                </h3>
         </Summary>
     )
 }
