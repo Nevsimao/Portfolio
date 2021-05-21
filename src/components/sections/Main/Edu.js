b035bf4dce60f1718858b1d54e9306e38aaeb631
Nevsimao <coday.ink@gmail.com>
2021-05-21 14:44:22 -0400

    started a edu comp

---
+++ src/components/sections/Main/Edu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { educationSec } from '../../../styles/Styles'
+
+const Edu = () => {
+    return (
+        <educationSec>
+            <h3>Education:</h3>
+        </educationSec>
+    )
+}
+
+export default Edu
